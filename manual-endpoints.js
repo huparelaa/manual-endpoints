f0dc067eadd8479a8a30834a23c3b3e9ffa0ab08
Hobarlan Uparela Arroyo <hobarlan.uparela@emast.co>
2026-05-14 10:48:20 -0500

    Add CORS middleware to handle cross-origin requests

--- manual-endpoints.js
+++ manual-endpoints.js
@@ -63,6 +63,15 @@ function writeRequestLog({ req, statusCode, durationMs, startedAt }) {
 // Middleware para parsear JSON
 app.use(express.json());
 
+// Middleware CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
 // Middleware para registrar cada peticion HTTP en archivos txt
 app.use((req, res, next) => {
   const startedAt = new Date();
